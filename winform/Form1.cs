7cfc54106389c9e617c9f55cea4bff67b3023ec1
Florian Leroux <florian.leroux3@laposte.net>
2021-03-08 17:34:10 +0100

    feat(winform): added material ui support

--- winform/Form1.cs
+++ winform/Form1.cs
@@ -6,36 +6,56 @@ using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 using System.ComponentModel;
 using System.Collections.Generic;
+using MaterialSkin.Controls;
+using MaterialSkin;
 
 namespace winform
 {
-    public partial class Form1 : Form
+    public partial class Form1 : MaterialForm
     {
         public Form1()
         {
             InitializeComponent();
+
+            var materialSkinManager = MaterialSkinManager.Instance;
+            materialSkinManager.AddFormToManage(this);
+            materialSkinManager.Theme = MaterialSkinManager.Themes.LIGHT;
+            materialSkinManager.ColorScheme = new ColorScheme(Primary.BlueGrey800, Primary.BlueGrey900, Primary.BlueGrey500, Accent.LightBlue200, TextShade.WHITE);
+
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
-            var url = "https://localhost:44343/weatherforecast";
-            var webRequest = (HttpWebRequest)WebRequest.Create(url);
-            var webResponse = (HttpWebResponse)webRequest.GetResponse();
+            try {
+                var url = "https://localhost:44343/weatherforecast";
+                var webRequest = (HttpWebRequest)WebRequest.Create(url);
+                var webResponse = (HttpWebResponse)webRequest.GetResponse();
 
-            if ((webResponse.StatusCode == HttpStatusCode.OK))
-            {
-                /*var reader = new StreamReader(webResponse.GetResponseStream());
-                string s = reader.ReadToEnd();
-                var arr = JsonConvert.DeserializeObject<List<Dto>>(s);
-                var list = new BindingList<Dto>(arr);
-                var data = new BindingSource(list, null);
-                dataGridView1.DataSource = data;*/
+                if ((webResponse.StatusCode == HttpStatusCode.OK))
+                {
+                    /*var reader = new StreamReader(webResponse.GetResponseStream());
+                    string s = reader.ReadToEnd();
+                    var arr = JsonConvert.DeserializeObject<List<Dto>>(s);
+                    var list = new BindingList<Dto>(arr);
+                    var data = new BindingSource(list, null);
+                    dataGridView1.DataSource = data;*/
 
+                }
+                else
+                {
+                    MessageBox.Show(string.Format("Status code == {0}", webResponse.StatusCode));
+                }
             }
-            else
+            catch (Exception exception)
             {
-                MessageBox.Show(string.Format("Status code == {0}", webResponse.StatusCode));
+                Console.WriteLine(exception.Message);
             }
+            
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
         }
     }
 }
