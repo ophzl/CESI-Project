70190dde47d745624278c7231cf3e5801ce03cef
Florian LEROUX <florian.leroux3@laposte.net>
2021-03-08 13:50:19 +0100

    feat(web): init scaffold

---
+++ web/Models/PurchaseOrder.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+namespace web.Models
+{
+    public class PurchaseOrder
+    {
+        public long Id { get; set; }
+        public double Price { get; set; }
+
+        public long SupplierId { get; set; }
+        public List<Product> Products { get; set; }
+    }
+}
---
+++ web/Models/WineFamily.cs
@@ -0,0 +1,9 @@
+﻿namespace web.Models
+{
+
+    public class WineFamily
+    {
+        public long Id { get; set; }
+        public string Name { get; set; }
+    }
+}
