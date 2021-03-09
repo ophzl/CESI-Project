58d531859c09c2abc5e7137da97d3f0476e06578
Florian Leroux <florian.leroux3@laposte.net>
2021-03-09 10:01:03 +0100

    revert(winform): changed hand

--- winform/Form1.Designer.cs
+++ winform/Form1.Designer.cs
@@ -51,7 +51,7 @@ namespace winform
             this.materialFlatButton1.BackColor = System.Drawing.SystemColors.ControlDarkDark;
             this.materialFlatButton1.Depth = 0;
             this.materialFlatButton1.Icon = null;
-            this.materialFlatButton1.Location = new System.Drawing.Point(6, 387);
+            this.materialFlatButton1.Location = new System.Drawing.Point(7, 501);
             this.materialFlatButton1.Margin = new System.Windows.Forms.Padding(4, 6, 4, 6);
             this.materialFlatButton1.MouseState = MaterialSkin.MouseState.HOVER;
             this.materialFlatButton1.Name = "materialFlatButton1";
@@ -69,7 +69,7 @@ namespace winform
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.RowHeadersWidth = 51;
             this.dataGridView1.RowTemplate.Height = 24;
-            this.dataGridView1.Size = new System.Drawing.Size(1071, 349);
+            this.dataGridView1.Size = new System.Drawing.Size(1071, 472);
             this.dataGridView1.TabIndex = 2;
             // 
             // materialTabControl1
@@ -84,7 +84,7 @@ namespace winform
             this.materialTabControl1.MouseState = MaterialSkin.MouseState.HOVER;
             this.materialTabControl1.Name = "materialTabControl1";
             this.materialTabControl1.SelectedIndex = 0;
-            this.materialTabControl1.Size = new System.Drawing.Size(1094, 467);
+            this.materialTabControl1.Size = new System.Drawing.Size(1094, 572);
             this.materialTabControl1.TabIndex = 3;
             // 
             // wines
@@ -92,11 +92,11 @@ namespace winform
             this.wines.Controls.Add(this.materialFlatButton2);
             this.wines.Controls.Add(this.dataGridView1);
             this.wines.Controls.Add(this.materialFlatButton1);
-            this.wines.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.wines.Cursor = System.Windows.Forms.Cursors.Arrow;
             this.wines.Location = new System.Drawing.Point(4, 25);
             this.wines.Name = "wines";
             this.wines.Padding = new System.Windows.Forms.Padding(3);
-            this.wines.Size = new System.Drawing.Size(1086, 438);
+            this.wines.Size = new System.Drawing.Size(1086, 543);
             this.wines.TabIndex = 0;
             this.wines.Text = "Vins";
             this.wines.UseVisualStyleBackColor = true;
@@ -108,7 +108,7 @@ namespace winform
             this.materialFlatButton2.BackColor = System.Drawing.SystemColors.ControlDarkDark;
             this.materialFlatButton2.Depth = 0;
             this.materialFlatButton2.Icon = null;
-            this.materialFlatButton2.Location = new System.Drawing.Point(1002, 387);
+            this.materialFlatButton2.Location = new System.Drawing.Point(1005, 501);
             this.materialFlatButton2.Margin = new System.Windows.Forms.Padding(4, 6, 4, 6);
             this.materialFlatButton2.MouseState = MaterialSkin.MouseState.HOVER;
             this.materialFlatButton2.Name = "materialFlatButton2";
@@ -120,42 +120,42 @@ namespace winform
             // 
             // clients
             // 
-            this.clients.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.clients.Cursor = System.Windows.Forms.Cursors.Default;
             this.clients.Location = new System.Drawing.Point(4, 25);
             this.clients.Name = "clients";
             this.clients.Padding = new System.Windows.Forms.Padding(3);
-            this.clients.Size = new System.Drawing.Size(793, 438);
+            this.clients.Size = new System.Drawing.Size(1086, 543);
             this.clients.TabIndex = 1;
             this.clients.Text = "Clients";
             this.clients.UseVisualStyleBackColor = true;
             // 
             // Wine_Family
             // 
-            this.Wine_Family.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.Wine_Family.Cursor = System.Windows.Forms.Cursors.Default;
             this.Wine_Family.Location = new System.Drawing.Point(4, 25);
             this.Wine_Family.Name = "Wine_Family";
             this.Wine_Family.Padding = new System.Windows.Forms.Padding(3);
-            this.Wine_Family.Size = new System.Drawing.Size(793, 438);
+            this.Wine_Family.Size = new System.Drawing.Size(1086, 543);
             this.Wine_Family.TabIndex = 2;
             this.Wine_Family.Text = "Familles de vin";
             this.Wine_Family.UseVisualStyleBackColor = true;
             // 
             // suppliers
             // 
-            this.suppliers.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.suppliers.Cursor = System.Windows.Forms.Cursors.Default;
             this.suppliers.Location = new System.Drawing.Point(4, 25);
             this.suppliers.Name = "suppliers";
-            this.suppliers.Size = new System.Drawing.Size(793, 438);
+            this.suppliers.Size = new System.Drawing.Size(1086, 543);
             this.suppliers.TabIndex = 3;
             this.suppliers.Text = "Fournisseurs";
             this.suppliers.UseVisualStyleBackColor = true;
             // 
             // suppliers_orders
             // 
-            this.suppliers_orders.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.suppliers_orders.Cursor = System.Windows.Forms.Cursors.Default;
             this.suppliers_orders.Location = new System.Drawing.Point(4, 25);
             this.suppliers_orders.Name = "suppliers_orders";
-            this.suppliers_orders.Size = new System.Drawing.Size(793, 438);
+            this.suppliers_orders.Size = new System.Drawing.Size(1086, 543);
             this.suppliers_orders.TabIndex = 4;
             this.suppliers_orders.Text = "Commandes fournisseurs";
             this.suppliers_orders.UseVisualStyleBackColor = true;
@@ -163,6 +163,7 @@ namespace winform
             // materialTabSelector1
             // 
             this.materialTabSelector1.BaseTabControl = this.materialTabControl1;
+            this.materialTabSelector1.Cursor = System.Windows.Forms.Cursors.Hand;
             this.materialTabSelector1.Depth = 0;
             this.materialTabSelector1.Location = new System.Drawing.Point(-1, 64);
             this.materialTabSelector1.MouseState = MaterialSkin.MouseState.HOVER;
@@ -176,7 +177,7 @@ namespace winform
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1092, 577);
+            this.ClientSize = new System.Drawing.Size(1092, 679);
             this.Controls.Add(this.materialTabSelector1);
             this.Controls.Add(this.materialTabControl1);
             this.Name = "Wine";
