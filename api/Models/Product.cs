06c6f2c56efee070a0a0f8f0fe209d3075758a54
Florian Leroux <florian.leroux3@laposte.net>
2021-03-09 20:10:38 +0100

    ref(api|models): refactor product model

--- api/Models/Product.cs
+++ api/Models/Product.cs
@@ -6,10 +6,17 @@ namespace api.Models
 
         public string Name { get; set; }
 
+        public string House { get; set; }
+        public string Year { get; set; }
+
         public double Price { get; set; }
         public double SellPrice { get; set; }
 
+        public long Quantity { get; set; }
+
         public WineFamily WineFamily { get; set; }
         public long WineFamily_Id { get; set; }
+
+        
     }
 }
