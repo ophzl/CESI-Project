f9a4e57ce289d00942a28a237bad4de1ff05eb3c
Florian Leroux <florian.leroux3@laposte.net>
2021-03-09 09:55:15 +0100

    feat(theme): changed winform theme

--- winform/Form1.cs
+++ winform/Form1.cs
@@ -21,7 +21,7 @@ namespace winform
             var materialSkinManager = MaterialSkinManager.Instance;
             materialSkinManager.AddFormToManage(this);
             materialSkinManager.Theme = MaterialSkinManager.Themes.LIGHT;
-            materialSkinManager.ColorScheme = new ColorScheme(Primary.BlueGrey800, Primary.BlueGrey900, Primary.BlueGrey500, Accent.LightBlue200, TextShade.WHITE);
+            materialSkinManager.ColorScheme = new ColorScheme(Primary.Purple800, Primary.Purple900, Primary.Purple500, Accent.Purple200, TextShade.WHITE);
 
         }
 
